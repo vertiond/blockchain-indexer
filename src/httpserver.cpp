a47decb930d2fc6786e2afec7175b1d6764fea5f
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2019-09-26 13:58:22 +0200

    Allow configuring user,password and port for RPC

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -40,7 +40,7 @@ VtcBlockIndexer::HttpServer::HttpServer(shared_ptr<leveldb::DB> db, shared_ptr<V
     this->mempoolMonitor = mempoolMonitor;
     blockReader.reset(new VtcBlockIndexer::BlockReader(blocksDir));
     scriptSolver = std::make_unique<VtcBlockIndexer::ScriptSolver>();
-    httpClient.reset(new jsonrpc::HttpClient("http://middleware:middleware@" + std::string(std::getenv("COIND_HOST")) + ":8332"));
+    httpClient.reset(new jsonrpc::HttpClient("http://" + std::string(std::getenv("COIND_RPCUSER")) + ":" + std::string(std::getenv("COIND_RPCPASSWORD")) + "@" + std::string(std::getenv("COIND_HOST")) + ":" + std::string(std::getenv("COIND_RPCPORT"))));
     vertcoind.reset(new VertcoinClient(*httpClient));
 }
 
